c403d0dc0ed8b47eeb5f513c6fee4343df30e993
Ravi Kishan Raj <48170320+rkraj624@users.noreply.github.com>
2022-04-29 16:15:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-if(process.env.Node_ENV==='production'){
+if(process.env.NODE_ENV=='production'){
   const path = require('path')
 
   app.get('/',(req,res)=>{
